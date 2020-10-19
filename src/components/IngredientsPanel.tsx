7691be10131b26373d118bfd4de7ae98a575684e
Andrew Dillon <andrew.dillon.j@gmail.com>
2020-10-19 17:26:24 -0500

    Cleanup

--- src/components/IngredientsPanel.tsx
+++ src/components/IngredientsPanel.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
--- src/components/SandwichesPanel.tsx
+++ src/components/SandwichesPanel.tsx
@@ -8,7 +8,6 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import { fetchSandwiches, postSandwich } from 'requests';
